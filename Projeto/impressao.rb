9e24f8c755bad04486bb27091f28b6aed5a4d7d4
RafaelNC <rafanonato_cardoso@hotmail.com>
2017-10-23 03:10:25 -0200

    Finalizando

--- Projeto/impressao.rb
+++ Projeto/impressao.rb
@@ -1,8 +1,12 @@
 load 'Classes/classeAnalise.rb'
 teste = Analise.new
-puts "Digite 1 para relatório total e 2 para ranking:"
+puts "Escolha a opção desejada:"
+puts "1 - Imprime relatório de cada jogo"
+puts "2 - Imprime Ranking por mortes"
+puts "3 - Executa as duas funções acima"
+print "Opção desejada:"
 y = gets
-while y.to_i != 1 and y.to_i != 2 do
+while y.to_i != 1 and y.to_i != 2 and y.to_i != 3 do
 	puts "Valor invalido!! Tente novamente:"
 	puts "Digite 1 para relatório total e 2 para ranking:"
 	y = gets
@@ -10,6 +14,9 @@ end
 if y.to_i == 1
 	teste.relatorio
 elsif y.to_i == 2
+	teste.ranking
+elsif
+	teste.relatorio
 	teste.ranking
 else
 	puts "Valor invalido!!"
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ParserCloud
+Parser para arquivo de log do jogo Quake3 Arena
+
+Imprime informações de cada jogo e Ranking de jogadores por kills
+
+#Feito em RUBY
